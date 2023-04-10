157d4d6c3ac8dab42f62b2c431c76972d0cbafaa
ShevchukA <antoshka.vbg@mail.ru>
2023-04-10 14:31:07 +0300

    feat: add card component template

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Card from "../card/Card";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">HEADER</header>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,7 @@
+import "./Card.scss";
+
+function Card() {
+  return <div className="card">CARD</div>;
+}
+
+export default Card;
